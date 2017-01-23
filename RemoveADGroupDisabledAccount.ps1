0e0e55558decae7401508ad80ac52d6feea3453d
Francis Paulo Andres <fbandres17@gmail.com>
2017-01-23 21:55:33 +0800

    Add files via upload

---
+++ RemoveADGroupDisabledAccount.ps1
@@ -0,0 +1,6 @@
+﻿
+# List all User accounts under specific AD Group and show only all disabled user accounts.
+$DisabledUser = Get-ADGroupMember -Identity "Domain Admins" | Get-ADUser | Where-Object {$_.Enabled -eq $false}
+
+# Remove all the filtered user accounts
+Remove-ADGroupMember -Identity "Domain Admins" -Members $DisabledUser
